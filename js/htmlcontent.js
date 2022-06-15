e6b7308a3a5f4c62f66c0ba60e8356da91d6f1af
Vikrant3d <vikrantthakur15@gmail.com>
2022-06-15 18:55:23 +0530

    ok

--- js/htmlcontent.js
+++ js/htmlcontent.js
@@ -4,13 +4,13 @@ var header = '<header class="main-header">'+
         '<div class="container">'+
           '<div class="navbar-header">'+
             '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"'+
-              'aria-expanded="false" aria-controls="navbar">'+
+              'aria-expanded="false" aria-controls="navbar" onclick="return hideLogo()">'+
               '<span class="sr-only">Toggle navigation</span>'+
               '<span class="icon-bar"></span>'+
               '<span class="icon-bar"></span>'+
               '<span class="icon-bar"></span>'+
             '</button>'+
-            '<a class="navbar-brand" href="index.html"><img src="assets/images/logo.jpg" style="margin-top:-10px" alt=""></a>'+
+            '<a class="navbar-brand" href="index.html"><img src="assets/images/logo.jpg" style="margin-top:-10px"  id="logoimg" alt=""></a>'+
           '</div>'+
           '<div id="navbar" class="navbar-collapse collapse pull-right">'+
             '<ul class="nav navbar-nav" id="menuid">'+
@@ -86,4 +86,7 @@ var header = '<header class="main-header">'+
 		$(this).addClass("is-active");
 		}
 	});	 
+ }
+ function hideLogo(){	 
+	 $("#logoimg").addClass("height55")
  }
