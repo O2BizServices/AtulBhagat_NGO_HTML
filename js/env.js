263419c717ab510c790122487032d746854812d0
Vikrant3d <vikrantthakur15@gmail.com>
2022-06-27 15:56:26 +0530

    ok

--- js/env.js
+++ js/env.js
@@ -1,2 +1,2 @@
-var contextPath="https://36n00y4czc.execute-api.ap-northeast-1.amazonaws.com/dev/api/v1/"; //DEV
-//var contextPath="https://k0jkw5q0c2.execute-api.ap-northeast-1.amazonaws.com/prod/api/v1/"; //PROD
+//var contextPath="https://36n00y4czc.execute-api.ap-northeast-1.amazonaws.com/dev/api/v1/"; //DEV
+var contextPath="https://k0jkw5q0c2.execute-api.ap-northeast-1.amazonaws.com/prod/api/v1/"; //PROD
